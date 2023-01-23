0b10fb9ccb4554d707018887e243b81abe329b9b
Anthony Giniers <antogyn@gmail.com>
2023-01-23 13:41:37 +0100

    feat: add support for a local path

--- README.md
+++ README.md
@@ -2,7 +2,17 @@
 
 A proxy from localhost to any remote http address, made with deno.
 
-Example:
+## Flags
+
+### --port (required)
+
+### --target (required)
+
+### --path (optional)
+
+## Example
 ```
-deno run --allow-net https://deno.land/x/localproxy/proxy.ts --port=3000 --target=https://github.com/antogyn/local-proxy
+deno run --allow-net https://deno.land/x/localproxy/proxy.ts --port=3000 --target=https://github.com/antogyn/local-proxy --path=/github
+
+open localhost:3000/github
 ```
--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,10 @@
-import { proxy } from "https://deno.land/x/oak_http_proxy@2.1.0/mod.ts";
-import { Application } from "https://deno.land/x/oak@v10.1.0/mod.ts";
 import { parse } from "https://deno.land/std@0.168.0/flags/mod.ts";
+import { Application } from "https://deno.land/x/oak@v10.1.0/mod.ts";
+import { proxy } from "https://deno.land/x/oak_http_proxy@2.1.0/mod.ts";
 
 async function start() {
   const flags = parse(Deno.args, {
-    string: ["port", "target"],
+    string: ["port", "target", "path"],
   });
 
   if (!flags.port || !flags.target) {
@@ -17,7 +17,23 @@ async function start() {
 
   const app = new Application();
 
-  app.use(proxy((ctx) => new URL(flags.target! + ctx.request.url.pathname)));
+  app.use((ctx, next) => {
+    const pathname: string = ctx.request.url.pathname;
+    if (flags.path) {
+      if (pathname.startsWith(flags.path)) {
+        return proxy(() => {
+          return new URL(flags.target! + pathname.slice(flags.path!.length));
+        })(ctx, next);
+      } else {
+        ctx.response.status = 404;
+        next();
+      }
+    } else {
+      return proxy(() => {
+        return new URL(flags.target! + pathname);
+      })(ctx, next);
+    }
+  });
 
   app.addEventListener("listen", ({ hostname, port, secure }) => {
     console.log(
