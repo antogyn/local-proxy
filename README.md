ca2e269da4b22d22ef901e190d3bdb4e144d9ceb
Anthony Giniers <antogyn@gmail.com>
2023-01-23 12:04:21 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Local-proxy
+
+A proxy from localhost to any remote http address, made with deno.
---
+++ proxy.ts
@@ -0,0 +1,33 @@
+import { proxy } from "https://deno.land/x/oak_http_proxy@2.1.0/mod.ts";
+import { Application } from "https://deno.land/x/oak@v10.1.0/mod.ts";
+import { parse } from "https://deno.land/std@0.168.0/flags/mod.ts";
+
+async function start() {
+  const flags = parse(Deno.args, {
+    string: ["port", "target"],
+  });
+
+  if (!flags.port || !flags.target) {
+    console.log("Missing --port and/or --target options");
+    console.log(
+      `Example: deno run --allow-net proxy.ts --port=3000 --target=https://github.com/oakserver/oak`
+    );
+    return;
+  }
+
+  const app = new Application();
+
+  app.use(proxy(flags.target));
+
+  app.addEventListener("listen", ({ hostname, port, secure }) => {
+    console.log(
+      `Listening on: ${secure ? "https://" : "http://"}${
+        hostname ?? "localhost"
+      }:${port}`
+    );
+  });
+
+  await app.listen({ port: Number(flags.port) });
+}
+
+start();
